41ba2e01d5754f81a39c3aec8026dba6ff12714b
AD <silentflute.ad@gmail.com>
2024-08-15 09:42:15 +0100

    adding a pass by argument, and an incrementer

--- helloworld.rb
+++ helloworld.rb
@@ -6,14 +6,32 @@ $globalVar = 'ruby'
 
 # first class
 class FirstClass
+    @@callsMade = 0
     def initialize()
         @personName = "Smith"
     end
 
     def displayDetails()  
+        @@callsMade += 1
         puts "Hello Mr #@personName"
+        puts "Calls made: #@@callsMade"
+    end
+end
+
+class SecondClass
+    def initialize(name)
+        @name = name
+    end
+    
+    def displayDetails
+        puts "Hello #@name"
     end
 end
 
 obj = FirstClass.new()
 obj.displayDetails()
+obj.displayDetails()
+obj.displayDetails()
+
+obj1 = SecondClass.new("mike")
+obj1.displayDetails
